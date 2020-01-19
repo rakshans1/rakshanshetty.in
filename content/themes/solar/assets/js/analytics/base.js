0151899f2a026a01861d626988afd113797c7497
Rakshan Shetty <shetty.raxx555@gmail.com>
2020-01-19 15:15:45 +0530

    Analytics Updated

--- content/themes/solar/assets/js/analytics/base.js
+++ content/themes/solar/assets/js/analytics/base.js
@@ -55,10 +55,6 @@ const init = () => {
   // Initialize the command queue in case analytics.js hasn't loaded yet.
   window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
 
-  ga('create', 'UA-XXXXX-Y', 'auto');
-  ga('set', 'transport', 'beacon');
-  ga('send', 'pageview');
-
   createTracker();
   trackErrors();
   trackCustomDimensions();
